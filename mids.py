fc86ee0647dff23b67f13cb3e4dc49e7057bfc36
steinshark <everettperson@gmail.com>
2022-01-28 09:37:39 -0500

    new updates to mids

--- mids.py
+++ mids.py
@@ -1,9 +1,10 @@
 from requests_html import HTMLSession
 from requests import get, post
+from json import dumps
 form_url = "https://midsweb.usna.edu/ITSD/midsw/drgwq010$mids.actionquery"
 cookies = {
 "s_session_id" : "B369168123494312803B0F1665CD0026",
-"OAMAuthnCookie_midsweb.usna.edu:443" : "sgUG29TDdIefNfJsjb9oSTbwZ8G3z5P31j%2FWXPa0JqqsuRvCOQKiMY5GffHCDen7echLxNDAHM1ofKicGQV5%2F305CbjoxP5kIPjo5snZlng8dwB8nxYsHD6DD8ecjCDNkcm1ECxVcyqwLzuhkmcJLTtVxpadrkXGbf8poAJtWYmXP9MKoNV1miEvZjdmwrhd9as36TKv1Y0hPCVe8cCEZSgHSAPH9PPoVH4NRqGLSYFL09nJ6KELVBTEGMzu16VKdMeDlq74AEs3q2L8ayuS06oPX70nKC9pQ6Wx6TOhofpTy9V%2Fpni7tfjoZ71ktxO0nIJzrImAriqzVzv9bUjmhFMDjSzsz%2BEwM%2Fz7%2BOgH0IvnCv31AnFnLuD7gTepVijlk7J39FEG2i4G5nP5SCZy%2FckOZQhvIaiTOJhjPitX13GyJvmUlj2j7H5BUK54VH0okKcyQ4MSQbEd7qRCy0vAHzXGvcd7OE518FL0TLfbyAphx57j8TMlZCNnsq%2FuFeHIKiLDzQoNF6v9QuRdDuWxTpwVtcCPmRCu85vWgTzA5nkf%2BExvd6iRCSuPtqs9hVQ3pDN2%2Fyhi1HEDLShxSo3xeA%3D%3D",
+"OAMAuthnCookie_midsweb.usna.edu:443" : "zM7Ey0J0hg73mfJw4mSF71OfqFSL4eSQige%2Fzcqmw3wuh133c74c6uQUTEjbEl0yj9oWvp6r66lZay8PL1IBI%2BtePMlBM1G%2BrCpgT%2BNCf6AXJ3wA7fv2RumXboW2aBXOLEcKfEJeEnvPn0bQQGQCBLPrS8%2FdenBOFjXoYSQftr2dXSG51haCu%2BQOyWilk0Snf%2FyuZypIrNm9IEE%2F%2BjQX1CBPLldmfAprap4odqbjfeyrnRodR9Cgj9kQ8%2Fgopzej6GbMj5cmuCKRO0UaxFJgWQtKM4QQdV05bBKho1iny%2FmY6KL8WtStJ5m%2FDygRFp5Q%2BnyXJqeGZVDtVqBrP23w8e0bhDIzoYMdg2Se8XQVRArZ%2FQEkWEZee0pXz4egfYuJwRw9UatP%2F2gdz9zMzJx2qpzQDXNPWyo2tkdSwg8Dh%2FlzOQA5FvnIFFBZX8cbk7lvYD9Z6A%2BznzKOODfMzxQj4HvLLgWUBGsnrXqlsR5Ujb8t4ROUEz0%2Fv01D7k5KhC0zBS0I8SFKA5Fxi8f9uZ80%2BBdYLvjvk13rAFNO%2BT9LTuwuS1WDzkBrDKhmYTvVtxDGhvWBnBfX%2FGSjJvJGPTzojA%3D%3D",
 "JSESSIONID" : "60B15731C991F27CCBE64F20689575A5"
 }
 
@@ -26,8 +27,7 @@ data = {
 
 
 def pull_Brigade(cl=22,begin=6, thresh=1,file='brigade'):
-
-    file = open("22_test",'w')
+    mapper = {}
     n = 0
     data["P_ALPHA"] = cl*10000
     data["P_SECOF_COOF_SEBLDA_AC_YR"] = 2000 + cl
@@ -40,10 +40,12 @@ def pull_Brigade(cl=22,begin=6, thresh=1,file='brigade'):
             schedule = resp.text.split("<P><B>Free Periods: </B>\n")[1].split('\n')[0]
             if n % 100 == 0:
                 print(f"made it through {n} records")
-            file.write(f"{name}:{schedule}\n")
+            mapper[name] = schedule
         except IndexError:
             print(f"midn: {data['P_ALPHA']} not cooperating...")
         n += 1
+    return mapper
 
 if __name__ == "__main__":
-    pull_Brigade(thresh=1000)
+    output = open("brigdae_22_schedules",'w')
+    output.write(dumps(pull_Brigade(thresh=10)))
