a1e71e1342c61d363041662546fb4244f19a2d8e
Everett Stenberg <47328318+Steinshark@users.noreply.github.com>
2025-12-30 21:49:38 -1000

    Add TF-IDF

--- SimHash.py
+++ SimHash.py
@@ -3,19 +3,60 @@ import zlib
 import numpy
 import xxhash
 from collections import Counter 
+import os 
 
 #todo: remove the top-k tokens as stopwords since they add little info (i dont care about 'the')
 stopwords       = [] 
-
+EOT_TOKEN       = "<|endoftext|>"
 #Subject to change 
 hash_fn         = xxhash.xx3_64
 hash_len        = 64
 
+def clean_doc(contents:str):
+    #Lower 
+    contents    = contents.lower()
+    
+    #Remove whitespace idiocy
+    while "  " in contents:
+        contents = contents.replace("  ", " ")
+    while "\n\n" in working_text:
+        contents = contents.replace("\n\n", "\n")
+
+    return contents
+    
+
+    
+def word_doc_counter(ds_path:str):
+    
+    term_freqs      = {}
+    n_docs          = 0
+    
+    for document in os.listdir(ds_path):
+        contents         = open(document,'r').read()
+
+        documents        = contents.split(EOT_TOKEN)
+
+        for doc in documents:
+            contents         = clean_doc(contents)
+            words            = set(contents.split(" "))
+            n_docs           += 1
+            for word in words:
+                term_freqs[word] += 1
+    
+    return term_freqs, n_docs
+
+def tf_idf(term,term_count,n_terms,n_docs):
+    tf                 = term_count / n_terms 
+    idf                = numpy.log(word_occurences[term] / n_docs)
+
+return tf * idf
+    
+    
 #This function processess a single document for SimHash analysis 
-def SimHash_doc(doc:str):
+def SimHash_doc(doc:str,n_docs):
     
     #Tokenize
-    doc_tokens      = tokenize_document(doc)
+    doc_tokens      = tokenize_document(doc,n_docs)
     
     #Hash 
     document_hash   = hash_document(doc_tokens)
@@ -33,21 +74,15 @@ def bit_arr(hash_digest:int):
     
 #This function cononacalizes the contents of a document
 #and returns an array of scaled tokens based on word count (log count) 
-def tokenize_document(doc:str):
+def tokenize_document(doc:str,n_docs):
     
     #Lower 
-    working_text    = doc.lower()
-    
-    #Remove whitespace idiocy
-    while "  " in working_text:
-        working_text = working_text.replace("  ", " ")
-    while "\n\n" in working_text:
-        working_text = working_text.replace("\n\n", "\n")
+    working_text    = clean_doc(doc)
     
     #Split and count words 
     working_words   = working_text.split(" ")
     word_counts     = Counter(working_words)
-    log_word_counts = {word: 1 + numpy.log(count) for word,count in word_counts.items()}
+    log_word_counts = {word: tf_idf(word,count,len(working_words),n_docs) for word,count in word_counts.items()}
     
     #Get tokens
     encoded_words   = [word.encode() for word in working_words]
@@ -81,7 +116,8 @@ def hash_document(weighted_tokens:numpy.array):
     return document_hash
 
 if __name__ == "__main__":
+    counts,n_docs = word_doc_counter()
     document = None
-    tokenized = SimHash_doc(document)
+    tokenized = SimHash_doc(document,n_docs)
     print(tokenized)
  
